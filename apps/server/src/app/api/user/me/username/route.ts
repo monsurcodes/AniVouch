73cfdd50f0298a1c4992c93e16676ec1614683b1
monsurcodes <monsurcodes@gmail.com>
2026-01-31 02:34:33 +0530

    feat: implement POST route for updating user username with validation

---
+++ apps/server/src/app/api/user/me/username/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import { db } from "@/db";
+import { user as userTable } from "@/db/schemas/auth-schema";
+import { eq } from "drizzle-orm";
+import { usernameValidation } from "@repo/types/src/schemas/authValidation";
+import { getCurrentUser } from "@/lib/auth-utils";
+import { treeifyError } from "zod";
+
+export async function POST(request: Request) {
+	const { user } = await getCurrentUser();
+
+	if (!user) {
+		return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+	}
+
+	const body = await request.json();
+	if (!body.username) {
+		return NextResponse.json({ error: "Username is required" }, { status: 400 });
+	}
+
+	const result = usernameValidation.safeParse(body.username as string);
+
+	if (!result.success) {
+		return NextResponse.json({ error: treeifyError(result.error) }, { status: 400 });
+	}
+
+	const username = result.data;
+
+	try {
+		await db.update(userTable).set({ username }).where(eq(userTable.id, user.id));
+		return NextResponse.json({ message: "Username updated successfully" }, { status: 200 });
+	} catch (error) {
+		if (error instanceof Error) {
+			return NextResponse.json({ error: error.message }, { status: 500 });
+		}
+		return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+	}
+}
