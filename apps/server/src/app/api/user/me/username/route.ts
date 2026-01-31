fcdb44c2072b4bc158555218d300340641bc10b2
monsurcodes <monsurcodes@gmail.com>
2026-02-01 02:42:59 +0530

    feat: refactor error handling in username update route to use AppError class
    - route fails to send a valid error response if user enters a username that already exists in db

--- apps/server/src/app/api/user/me/username/route.ts
+++ apps/server/src/app/api/user/me/username/route.ts
@@ -4,24 +4,24 @@ import { user as userTable } from "@/db/schemas/auth-schema";
 import { eq } from "drizzle-orm";
 import { usernameValidation } from "@repo/types/src/schemas/authValidation";
 import { getCurrentUser } from "@/lib/auth-utils";
-import { treeifyError } from "zod";
+import { handleError, AppError } from "@/lib/error-handler";
 
 export async function POST(request: Request) {
 	const { user } = await getCurrentUser();
 
 	if (!user) {
-		return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+		return handleError(new AppError("Unauthorized", 401));
 	}
 
 	const body = await request.json();
 	if (!body.username) {
-		return NextResponse.json({ error: "Username is required" }, { status: 400 });
+		return handleError(new AppError("Username is required", 400));
 	}
 
 	const result = usernameValidation.safeParse(body.username as string);
 
 	if (!result.success) {
-		return NextResponse.json({ error: treeifyError(result.error) }, { status: 400 });
+		return handleError(result.error);
 	}
 
 	const username = result.data;
@@ -34,23 +34,20 @@ export async function POST(request: Request) {
 	}
 
 	try {
-		const existingUsername = await db
-			.select({
-				username: userTable.username,
-			})
-			.from(userTable)
-			.where(eq(userTable.username, username));
+		// const existingUsername = await db
+		// 	.select({
+		// 		username: userTable.username,
+		// 	})
+		// 	.from(userTable)
+		// 	.where(eq(userTable.username, username));
 
-		if (existingUsername.length > 0) {
-			return NextResponse.json({ error: "Username is already taken" }, { status: 409 });
-		}
+		// if (existingUsername.length > 0) {
+		// 	return NextResponse.json({ error: "Username is already taken" }, { status: 409 });
+		// }
 
 		await db.update(userTable).set({ username }).where(eq(userTable.id, user.id));
 		return NextResponse.json({ message: "Username updated successfully" }, { status: 200 });
 	} catch (error) {
-		if (error instanceof Error) {
-			return NextResponse.json({ error: error.message }, { status: 500 });
-		}
-		return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+		return handleError(error);
 	}
 }
