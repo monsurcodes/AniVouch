71777d8fb437d2a88e959a2e84b9cb4bf2623ca0
monsurcodes <monsurcodes@gmail.com>
2026-02-01 23:31:44 +0530

    feat: linter now optimizes imports on save

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,8 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import importPlugin from "eslint-plugin-import";
+import unusedImports from "eslint-plugin-unused-imports";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -27,6 +29,56 @@ const baseConfig = [
 	},
 	{
 		ignores: ["dist/**", ".next/**", "node_modules/**"],
+	},
+	{
+		plugins: {
+			import: importPlugin,
+			"unused-imports": unusedImports,
+		},
+		settings: {
+			"import/resolver": {
+				typescript: {
+					alwaysTryTypes: true,
+					project: "./tsconfig.json",
+				},
+			},
+		},
+		rules: {
+			// Remove unused imports automatically
+			"unused-imports/no-unused-imports": "error",
+			"unused-imports/no-unused-vars": [
+				"warn",
+				{
+					vars: "all",
+					varsIgnorePattern: "^_",
+					args: "after-used",
+					argsIgnorePattern: "^_",
+				},
+			],
+
+			// Enforce import order
+			"import/order": [
+				"error",
+				{
+					groups: [
+						"builtin", // Node built-in modules
+						"external", // npm packages
+						"internal", // Aliased modules
+						"parent", // Parent imports
+						"sibling", // Sibling imports
+						"index", // Index imports
+					],
+					"newlines-between": "always",
+					alphabetize: {
+						order: "asc",
+						caseInsensitive: true,
+					},
+				},
+			],
+
+			// Prevent duplicate imports
+			"import/no-duplicates": "error",
+		},
 	},
 	eslintConfigPrettier,
 ];
